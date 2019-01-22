10868a09a7f8f658365dffd17984272084c4ea26
DeKatze <phorlaender@gmail.com>
2019-01-22 18:17:19 +0100

    Commenting stuff in intitalize

--- Projekt/Chessboard.cpp
+++ Projekt/Chessboard.cpp
@@ -25,8 +25,14 @@ void Chessboard::initialize(std::ifstream& file)
 		char nextChar = file.get();
 		int pos = 0;
 
+		//lambda function as a variable for some counting stuff
 		auto getPos = [&]()
 		{
+			// interpreting the chess board as a sequence of fields,
+			// for each consecutive letter we add 8 to the count of the 
+			// next char in the file, which should be a number from 1 to 8.
+			// as a result we should get an index from 1-64 for each field, 
+			// from left to right from top to bottom.
 			switch (file.get())
 			{
 			case 'a':
@@ -62,23 +68,31 @@ void Chessboard::initialize(std::ifstream& file)
 			return pos;
 		};
 
+		//checks for the next char and executes different stuff 
 		switch (nextChar)
 		{
 		case 's':
+			// s for knight, dont ask
 			Knight* k();
 			m_board[getPos()]->setPiece(k);
 			break;
 		case 'b':
+			// b for pawn, dont ask
 			break;
 		case 'k':
+			// k for king, yey!
 			break;
 		case 'S':
+			// capital S for black knight, dont ask
 			break;
 		case 'B':
+			// capital B for black pawn, dont ask
 			break;
 		case 'K':
+			// capital K for black king, dont ask
 			break;
 		case ',':
+			// komma is a seperator
 			continue;
 		}
 	}
